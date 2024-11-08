87073aff5d73442d20721b1ee50e672d7992ebf8
palakgupta2712 <palakgupta2712@gmail.com>
2024-11-08 23:55:22 +0530

    cleanup commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Todo Project",
@@ -12,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable}`}>
       <head>
         <title>Introducing Todo Project</title>
       </head>
