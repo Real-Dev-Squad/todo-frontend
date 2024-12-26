a8e4dae7517bc50ac8174e4a9842028cb9d824fd
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2024-12-26 22:34:19 +0530

    Added tests for task page components

--- __tests__/integration/home.test.tsx
+++ __tests__/integration/home.test.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 import { expect, test } from "vitest";
-import Home from "../page";
+import Home from "@/app/page";
 
 test("renders the homepage correctly", () => {
   const { getByText } = render(<Home />);
