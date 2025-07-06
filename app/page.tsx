aac8fcf66803ebe081210c65381355b1699994e5
tejaskh3 <tejaskh3@gmail.com>
2025-07-06 14:01:35 +0530

    feat: add auth component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { AuthModal } from "@/components/auth/auth-modal";
 
 export default function Home() {
   return <div className="flex min-h-screen flex-col items-center py-8 ">
     <h1>Hello World!</h1>
-    <Button >Click Me</Button>
-
+    {/* TODO: Later this AuthModal will be used from header */}
+    <AuthModal>
+      <Button>Sign in</Button>
+    </AuthModal>
   </div>;
 }
