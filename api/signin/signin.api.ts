fa3886f68dfe87f8641d4f55b7de55ff059ce48c
tejaskh3 <tejaskh3@gmail.com>
2025-07-06 22:36:16 +0530

    fix: login page

--- api/signin/signin.api.ts
+++ api/signin/signin.api.ts
@@ -2,6 +2,6 @@ import { apiClient } from "@/utils/api-client"
 
 
 export const signInWithGoogle = async () => {
-  const response = await apiClient.get("/v1/auth/google/callback/")
+  const response = await apiClient.get("/v1/auth/google/login")
   return response.data
 }
