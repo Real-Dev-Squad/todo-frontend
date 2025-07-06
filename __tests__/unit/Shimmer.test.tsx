871a2be0c2e1eed8be1cf6d10aee8669abbc32de
tejaskh3 <tejaskh3@gmail.com>
2025-07-06 14:51:30 +0530

    add: test

--- __tests__/unit/Shimmer.test.tsx
+++ __tests__/unit/Shimmer.test.tsx
@@ -1,3 +1,4 @@
+import { Shimmer } from "@/components/shimmer/shimmer";
 import { ListShimmer } from "@/components/shimmer/ListShimmer";
 import { cleanup, render, screen } from "@testing-library/react";
 import { afterEach, describe, expect, test, vi } from "vitest";
@@ -8,6 +9,15 @@ afterEach(() => {
 });
 
 
+describe("Shimmer Component Unit Test", () => {
+  test("should render the shimmer animation when loading", () => {
+    render(
+      <Shimmer />
+    );
+    const shimmerElement = screen.getByTestId("shimmer");
+    expect(shimmerElement).not.toBeNull();
+  });
+})
 
 describe("List Shimmer Component Unit Test", () => {
 
