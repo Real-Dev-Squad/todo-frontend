b6d2399d1c653263cb1f7ee6b3a3b54e5b0cbb19
tejaskh3 <tejaskh3@gmail.com>
2025-07-06 15:19:54 +0530

    remove: test changes

--- __tests__/unit/Shimmer.test.tsx
+++ __tests__/unit/Shimmer.test.tsx
@@ -8,14 +8,15 @@ afterEach(() => {
   vi.resetAllMocks();
 });
 
-
 describe("Shimmer Component Unit Test", () => {
+
   test("should render the shimmer animation when loading", () => {
     render(
       <Shimmer />
     );
     const shimmerElement = screen.getByTestId("shimmer");
     expect(shimmerElement).not.toBeNull();
+
   });
 })
 
