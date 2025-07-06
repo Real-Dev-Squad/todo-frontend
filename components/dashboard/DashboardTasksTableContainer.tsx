103c2ba805e25d5bd969e4b755cb828e37894716
tejaskh3 <tejaskh3@gmail.com>
2025-07-06 19:54:57 +0530

    remove: border bottom

--- components/dashboard/DashboardTasksTableContainer.tsx
+++ components/dashboard/DashboardTasksTableContainer.tsx
@@ -5,7 +5,7 @@ import { DashboardTasksTableTabs } from "./DashboardTasksTableTabs";
 export const DashboardTasksTable = () => {
   return (
     <div className="lg:col-span-2 max-h-screen">
-      <div className="p-4 border-b">
+      <div className="p-4">
         <h2 className="text-2xl font-semibold mb-2">All Tasks</h2>
         <DashboardTasksTableTabs />
       </div>
