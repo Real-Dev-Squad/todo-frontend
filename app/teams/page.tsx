f87c7ee9a41d7148cc39670a754f032d2902f0bb
RishiChaubey31 <cenationrishi31@gmail.com>
2025-07-06 15:24:53 +0530

    initial commit

---
+++ app/teams/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/NavBar'
+import SideBar from '@/components/SideBar'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <SideBar />
+        <Navbar />
+    </div>
+  )
+}
+
+export default page
