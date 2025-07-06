8b819386c75b781344e9e04ffba7d37c6938a684
tejaskh3 <tejaskh3@gmail.com>
2025-07-06 16:24:22 +0530

    feat: add default options

--- app/_provider.tsx
+++ app/_provider.tsx
@@ -4,10 +4,19 @@ import {
     QueryClient,
   } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import { useState } from 'react';
 
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
- const queryClient = new QueryClient();
+    const [queryClient] = useState(() => new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 3,              
+            refetchOnMount: true,  
+            staleTime: 0,          
+          },
+        },
+      }));
+    
   return (
     <QueryClientProvider client={queryClient}>
       {children}
