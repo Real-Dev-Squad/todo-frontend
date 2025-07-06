8420906b131ae9a69613daf9e3e6aacd6271dc32
tejaskh3 <tejaskh3@gmail.com>
2025-07-06 17:30:34 +0530

    feat: add dashboard page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
+// "use client";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { QueryProvider } from "./_provider";
+import { SideBar } from "@/components/SideBar";
+import { NavBar } from "@/components/NavBar";
+import { ConditionalLayout } from "../components/ConditionalLayout";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,7 +30,9 @@ export default function RootLayout({
       
         <body>
           <QueryProvider>
-            {children}
+            <ConditionalLayout>
+              {children}
+            </ConditionalLayout>
           </QueryProvider>
         </body>
     </html>
