abfe9f041af4f62491e0b1b77edea5d9508cc6de
tejaskh3 <tejaskh3@gmail.com>
2025-07-06 14:39:12 +0530

    remove: test

--- __tests__/unit/Shimmer.test.tsx
+++ __tests__/unit/Shimmer.test.tsx
@@ -1,4 +1,3 @@
-import { Shimmer } from "@/components/shimmer/shimmer";
 import { ListShimmer } from "@/components/shimmer/ListShimmer";
 import { cleanup, render, screen } from "@testing-library/react";
 import { afterEach, describe, expect, test, vi } from "vitest";
@@ -8,17 +7,7 @@ afterEach(() => {
   vi.resetAllMocks();
 });
 
-describe("Shimmer Component Unit Test", () => {
 
-  test("should render the shimmer animation when loading", () => {
-    render(
-      <Shimmer />
-    );
-    const shimmerElement = screen.getByTestId("shimmer");
-    expect(shimmerElement).not.toBeNull();
-
-  });
-})
 
 describe("List Shimmer Component Unit Test", () => {
 
