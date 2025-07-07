f6655885611f95332ccf686675e78f03cbab7888
tejaskh3 <tejaskh3@gmail.com>
2025-07-08 01:06:46 +0530

    refactor: change url name

--- components/auth/signin-button.tsx
+++ components/auth/signin-button.tsx
@@ -33,7 +33,7 @@ export function SigninButton() {
         <div className="flex flex-col gap-4 py-4">
           <Link
             className="w-full bg-primary hover:bg-primary/90 text-white py-3 rounded-lg"
-            href={`${process.env.NEXT_PUBLIC_API_URL}/v1/auth/google/login`}
+            href={`${process.env.NEXT_BACKEND_API_URL}/v1/auth/google/login`}
             target="_blank"
           >
             Sign in with Google
