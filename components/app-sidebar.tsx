8bfdd25060fe3081fd1ead88c81faa46bf5807c0
yash <yashraj0207@outlook.com>
2025-07-14 04:24:58 +0530

    feat: enhance AppSidebar with dynamic team links and loading state
    - Integrated Teams API to fetch team data for sidebar links.
    - Added a loading state with a shimmer effect while fetching teams.
    - Refactored sidebar link rendering to accommodate dynamic team links.
    - Improved sidebar structure with new components for better organization.

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { TeamsApi } from '@/api/teams/teams.api'
+import { GetTeamsDto } from '@/api/teams/teams.type'
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
@@ -12,11 +15,90 @@ import {
   SidebarRail,
 } from '@/components/ui/sidebar'
 import { appConfig } from '@/config/app-config'
-import { SIDEBAR_LINKS } from '@/config/sidebar'
+import { SIDEBAR_LINKS, TSidebarLink } from '@/config/sidebar'
+import { useQuery } from '@tanstack/react-query'
 import { usePathname } from 'next/navigation'
+import { Shimmer } from './Shimmer'
+
+const getSidebarLinks = (teams?: GetTeamsDto): TSidebarLink[] => {
+  if (!teams || teams.teams.length === 0) {
+    return SIDEBAR_LINKS
+  }
+
+  const sidebarLinks = SIDEBAR_LINKS.filter((link) => link.url !== '/teams')
+
+  const teamsLinks = teams.teams.map((team) => ({
+    title: team.name,
+    url: `/teams/${team.id}`,
+  }))
+
+  return [
+    ...sidebarLinks,
+    {
+      title: 'Teams',
+      url: '#',
+      items: teamsLinks,
+    },
+  ]
+}
+
+const SidebarShimmer = () => {
+  return (
+    <>
+      {new Array(4).fill(0).map((_, index) => (
+        <SidebarMenuItem key={index}>
+          <SidebarMenuButton asChild>
+            <Shimmer className="h-8 w-full bg-gray-200" />
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ))}
+    </>
+  )
+}
+
+type SidebarLinkProps = {
+  link: TSidebarLink
+  isActive: boolean
+}
+
+const SidebarLink = ({ link, isActive }: SidebarLinkProps) => {
+  if (link.items) {
+    return (
+      <SidebarGroup>
+        <SidebarGroupLabel>{link.title}</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {link.items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild isActive={isActive}>
+                  <a href={item.url}>{item.title}</a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    )
+  }
+
+  return (
+    <SidebarMenuItem className="px-2">
+      <SidebarMenuButton asChild isActive={isActive}>
+        <a href={link.url}>{link.title}</a>
+      </SidebarMenuButton>
+    </SidebarMenuItem>
+  )
+}
 
 export const AppSidebar = ({ ...props }: React.ComponentProps<typeof Sidebar>) => {
   const pathname = usePathname()
+
+  const { data, isLoading } = useQuery({
+    queryKey: TeamsApi.getTeams.key,
+    queryFn: TeamsApi.getTeams.fn,
+  })
+
+  const links = data
 
   return (
     <Sidebar {...props}>
@@ -28,13 +110,17 @@ export const AppSidebar = ({ ...props }: React.ComponentProps<typeof Sidebar>) =
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
-              {SIDEBAR_LINKS.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild isActive={pathname.startsWith(item.url)}>
-                    <a href={item.url}>{item.title}</a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {isLoading ? (
+                <SidebarShimmer />
+              ) : (
+                getSidebarLinks(data).map((item) => (
+                  <SidebarLink
+                    link={item}
+                    key={item.title}
+                    isActive={pathname.startsWith(item.url)}
+                  />
+                ))
+              )}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
