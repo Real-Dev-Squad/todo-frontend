1a12c8e291c8be3b3ffc7abdc2180400a7af999f
Mayank Bansal <mayankbansal125@gmail.com>
2025-07-17 01:42:30 +0530

    feat: integrate GET API for watchlisted tasks

---
+++ modules/dashboard/components/dashboard-watchlist-tasks-table.tsx
@@ -0,0 +1,40 @@
+import { DashboardTasksTable } from './dashboard-tasks-table'
+import { CommonPageError } from '@/components/common-page-error'
+import { Shimmer } from '@/components/Shimmer'
+import { useQuery } from '@tanstack/react-query'
+import { TasksApi } from '@/api/tasks/tasks.api'
+import { DashboardTasksTableTabs as TabsConstants } from '../constants'
+import { TTask } from '@/api/tasks/tasks.types'
+import { NUM_TASK_PRIORITY_TO_TASK_ENUM } from '@/api/tasks/tasks.enum'
+
+export const DashboardWatchlistTasksTable = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: TasksApi.getWatchListTasks.key,
+    queryFn: TasksApi.getWatchListTasks.fn,
+  })
+  const tasks = data?.tasks ?? null
+
+  if (isLoading) {
+    return <Shimmer className="h-48 w-full" />
+  }
+
+  if (isError) {
+    return <CommonPageError />
+  }
+
+  if (!tasks || tasks.length === 0) {
+    return (
+      <div className="flex h-48 w-full items-center justify-center">
+        No tasks added in watchlist
+      </div>
+    )
+  }
+
+  const formattedTasks: TTask[] = tasks.map((task) => ({
+    ...task,
+    id: task.taskId,
+    in_watchlist: true,
+    priority: NUM_TASK_PRIORITY_TO_TASK_ENUM[task.priority ?? 0],
+  }))
+  return <DashboardTasksTable type={TabsConstants.WatchList} tasks={formattedTasks} />
+}
