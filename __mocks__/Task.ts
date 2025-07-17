ad3cdb16ecc62ce5e7d33fc345923b45c6986d49
RishiChaubey31 <cenationrishi31@gmail.com>
2025-07-18 01:41:28 +0530

    chore/added-mock-data-testing

---
+++ __mocks__/Task.ts
@@ -0,0 +1,110 @@
+import { TASK_PRIORITY_ENUM, TASK_STATUS_ENUM } from '@/api/tasks/tasks.enum'
+import { TTask } from '@/api/tasks/tasks.types'
+import { TUser } from '@/api/users/users.types'
+
+export const dummyUsers: TUser[] = [
+  {
+    userId: '1',
+    name: 'John Doe',
+    email: 'john.doe@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '2',
+    name: 'Jane Smith',
+    email: 'jane.smith@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '3',
+    name: 'Michael Johnson',
+    email: 'michael.johnson@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '4',
+    name: 'Emily Brown',
+    email: 'emily.brown@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '5',
+    name: 'David Wilson',
+    email: 'david.wilson@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '6',
+    name: 'Sarah Davis',
+    email: 'sarah.davis@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '7',
+    name: 'Robert Miller',
+    email: 'robert.miller@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '8',
+    name: 'Lisa Garcia',
+    email: 'lisa.garcia@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '9',
+    name: 'James Martinez',
+    email: 'james.martinez@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '10',
+    name: 'Jessica Anderson',
+    email: 'jessica.anderson@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '11',
+    name: 'Christopher Taylor',
+    email: 'christopher.taylor@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '12',
+    name: 'Ashley Thomas',
+    email: 'ashley.thomas@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '13',
+    name: 'Matthew Jackson',
+    email: 'matthew.jackson@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '14',
+    name: 'Amanda White',
+    email: 'amanda.white@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+  {
+    userId: '15',
+    name: 'Daniel Harris',
+    email: 'daniel.harris@gmail.com',
+    picture: 'https://example.com/john.jpg',
+  },
+]
+
+export const initialData: TTask = {
+  id: '1',
+  title: 'Sample Task',
+  description: 'This is a test task description.',
+  dueAt: '2024-12-31',
+  assignee: {
+    id: dummyUsers[0].userId,
+    name: dummyUsers[0].name,
+  },
+  status: TASK_STATUS_ENUM.TODO,
+  priority: TASK_PRIORITY_ENUM.LOW,
+  tags: ['Urgent'],
+}
