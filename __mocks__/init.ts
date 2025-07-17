cbbebf934fd021b25441bee38d26e6866b38e96b
Hariom Vashista <rishivashista4@gmail.com>
2025-07-18 04:08:59 +0530

    feat(msw): remove handlers for APIs - separate ticket will be create for them - refactored msw setup

---
+++ __mocks__/init.ts
@@ -0,0 +1,33 @@
+export async function enableMocking() {
+  const isDev = process.env.NODE_ENV === 'development'
+  const isMockingEnabled = process.env.NEXT_PUBLIC_API_MOCKING === 'true'
+
+  if (!isDev || !isMockingEnabled) {
+    return
+  }
+  try {
+    if (typeof window === 'undefined') {
+      const { server } = await import('./server')
+      server.listen({
+        onUnhandledRequest: (req, print) => {
+          if (req.url.includes('_next')) {
+            return
+          }
+          print.warning()
+        },
+      })
+    } else {
+      const { worker } = await import('./browser')
+      await worker.start({
+        onUnhandledRequest: (req, print) => {
+          if (req.url.includes('_next')) {
+            return
+          }
+          print.warning()
+        },
+      })
+    }
+  } catch (error) {
+    console.log('Failed to start MSW server: ', error)
+  }
+}
